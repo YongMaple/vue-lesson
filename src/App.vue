f1210356dff1cb97aafa39a8f35493dc0a3f615f
YongMaple <jymaple@gmail.com>
2017-03-09 16:38:38 +0800

    获取用户输入数据

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,36 @@
 <template>
     <div>
         <label>姓名：</label>
-        <input type="text"><br>
+        <input type="text" v-model="form.name"><br>
         <label>年龄：</label>
-        <input type="number"><br>
+        <input type="number" v-model="form.age"><br>
         <label>性别：</label>
-        <select>
+        <select v-model="form.gender">
             <option value="">请选择</option>
             <option value="male">男</option>
             <option value="female">女</option>
         </select><br>
-        <button type="button">添加</button>
+        <button type="button" @click="submit">添加</button>
     </div>
 </template>
 
 <script>
+export default {
+    data(){
+        return {
+            form:{
+                name:'',
+                age:'',
+                gender:''
+            }
+        }
+    },
+    methods:{
+       submit(){
+           alert(`输入的姓名：${ this.form.name }，输入的年龄：${ this.form.age }，选择的性别：${ this.form.gender }`)
+       }
+   }
+}
 </script>
 
 <style>
