72ecd7c1dc881819512e3ff4cfacc9e38a59a710
YongMaple <jymaple@gmail.com>
2017-03-09 16:58:12 +0800

    尝试添加表单校验

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <template>
 <div>
     <label>姓名：</label>
-    <input type="text" v-model="form.name"><br>
+    <input type="text" v-model="form.name.value">
+    <span v-if="form.name.valid">正确</span>
+    <span v-else>正确</span>
+    <br>
     <label>年龄：</label>
-    <input type="number" v-model="form.age"><br>
+    <input type="number" v-model="form.age.value"><br>
     <label>性别：</label>
-    <select v-model="form.gender">
+    <select v-model="form.gender.value">
             <option value="">请选择</option>
             <option value="male">男</option>
             <option value="female">女</option>
@@ -13,36 +16,53 @@
     <button type="button" @click="submit">添加</button>
 </div>
 </template>
-
 <script>
 export default {
     data() {
         return {
             form: {
-                name: '',
-                age: '',
-                gender: ''
+                name: {
+                    value: '',
+                    valid: false
+                },
+                age: {
+                    value: '',
+                    valid: false
+                },
+                gender: {
+                    value: '',
+                    valid: false
+                }
             }
         }
     },
     methods: {
         submit() {
+            const formDate = {
+                name : this.form.name.value,
+                age : this.form.age.value,
+                gender : this.form.gender.value
+            }
             fetch('http://localhost:3000/user', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
                 },
-                body: JSON.stringify(this.form)
+                body: JSON.stringify(formDate)
             }).then(() => {
                 alert("添加成功")
             }).catch(response => {
                 console.log(response);
             })
         }
+    },
+    computed: {
+        'form.name.valid': function() {
+            return this.form.name.value.length > 10 ? false : true
+        }
     }
 }
 </script>
-
 <style>
 
 </style>
